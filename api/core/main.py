5f6ca48787a211c8593fb046f5c3e67ade4de3c1
cotaByte <minanajoverrafa@gmail.com>
2024-04-08 09:58:47 +0200

    implement first functionalities

--- api/core/main.py
+++ api/core/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from fastapi import Body,FastAPI
+from typing import Annotated
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    return "ieeeie"
+    return "root api"
+
+#Auth region
+@app.post("/login")
+async def login(request: Annotated[object,Body()]):
+    print(request)
+    #TODO => IMPLEMENT LOGIN
+    
+#End Auth region
+
+#Expenses region
+@app.get("/expenses/{user_id}/{month}")
+async def getExpenses(user_id:int,month:int):
+    if(user_id==1 and month==1): return {"Object":"Object"}
+    #TODO=> IMPLEMENT EXPENSES
+
+
+    
+#End Expenses region
