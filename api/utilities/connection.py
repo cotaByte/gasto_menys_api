1f851776e34a952fbca38191c854b3abd11c10fc
cotaByte <minanajoverrafa@gmail.com>
2024-04-15 16:58:58 +0200

    new files

---
+++ api/utilities/connection.py
@@ -0,0 +1,33 @@
+import psycopg2
+
+
+# List of connections
+databases={
+    "GASTOMENYS":{
+        "host":"localhost",
+        "port":5432,
+        "username":"rafa",
+        "password":"X18kdXBQM3JwNCRzX18=",
+        "database":"GastoMenys"
+    }
+}
+
+
+def connect(bd:str):
+    
+    o_bd= databases[bd]
+    
+    if not o_bd: return {"ok":False,"msg":f"Unable to find database {bd}"}
+    
+    try:
+        con = psycopg2.connect(
+            host=o_bd["host"],
+            user=o_bd["username"],
+            password=o_bd["password"],
+            database=o_bd["database"]
+        )
+        return con
+    except Exception as ex:
+        print(ex)
+        return False
+
